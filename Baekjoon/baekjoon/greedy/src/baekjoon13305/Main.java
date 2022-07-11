3cfe8d691032ad9b8ff14152f198c2970885b8de
Seph-23 <seongmlyu95@gmail.com>
2022-07-11 11:37:25 +0900

    07-11-2022

---
+++ Baekjoon/baekjoon/greedy/src/baekjoon13305/Main.java
@@ -0,0 +1,22 @@
+package baekjoon13305;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        int numCity = 4;                //첫재쭐 인풋, 도시 갯수.
+        int[] distance = {2, 3, 1};     //도시 사이의 거리.
+        int[] price = {5, 2, 4, 1};     //도시 별 기름 가격. 이동할때 km당 l의 기름이 사용된다.
+        //제일 왼쪽 도시에서 제일 오른쪽 도시로 가는 최소 비용을 출력한다.
+        //시작할때의 기름은 0이다.
+        int gas = 0;
+        int totalPrice = 0;
+
+        for (int i = 0; i < numCity - 1; i++) {
+            if (i == 0) {
+                
+            }
+        }
+    }
+}
